a9cd73384926768f9495f489e861b80e1d4c08a2
Luiz588 <166726339+Luiz588@users.noreply.github.com>
2024-05-23 16:55:59 -0300

    Update app.js

--- login-cadastro/app.js
+++ login-cadastro/app.js
@@ -16,7 +16,7 @@ db.connect((error)=>{
     if(error){
         console.log('Erro ao conectar com o MySQL', error);
     } else{
-        console.log('Conectado ao ')
+        console.log('Conectado ao MySQL')
     }
 });
 
@@ -27,20 +27,23 @@ app.get("/", (req, res)=>{
 })
 
 app.post("/login", (req, res)=>{
-    const username = req.body.usuario
-    const password = req.body.senha
+    const username = req.body.username
+    const password = req.body.password
 
     db.query('SELECT password FROM user WHERE username = ?', [username], (error, results)=>{
         if (results.length > 0){
             const passwordBD = results[0].password;
-
+            if (password === passwordBD){
+                console.log("Login bem sucedido")
+            }else{
+                console.log("Senha incorreta")
+            }
         }else{
-            console.log('Usuario não cadastrado!')
+            console.log('Usuário não cadastrado!')
         }
     })
-   
 })
 
 app.listen(port, ()=>{
-    console.log(`Servidor rodando no endereçO: https://localhost:${port}`)
+    console.log(`Servidor rodando no endereço: http://localhost:${port}`)
 })
