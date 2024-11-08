28c60d70792b4d809a7dfdcbbcc2d83072f70cb2
Luiz588 <166726339+Luiz588@users.noreply.github.com>
2024-11-08 15:18:17 -0300

    Add files via upload

---
+++ jasmine-standalone-5.4.0/spec/testespec.js
@@ -0,0 +1,101 @@
+//NO TESTESPEC.JS VAI FICAR O TESTE
+
+// it("Lista de Teste", function (){
+//    expect(inteiro(1)).toBe(1);
+//    expect(inteiro(2)).toBe(2);
+//    expect(inteiro(3)).toBe(3);
+//    expect(inteiro(4)).toBe(4);
+// })
+
+// describe("Teste de números", function(){
+//    it("Fizz Buzz", function () {
+//        expect(jogo(1)).toBe(1);
+//        expect(jogo(2)).toBe(2);
+//        expect(jogo(3)).toBe("Fizz");
+//        expect(jogo(4)).toBe(4);
+//        expect(jogo(5)).toBe("Buzz");
+//        expect(jogo(6)).toBe("Fizz");
+//        expect(jogo(7)).toBe(7);
+//        expect(jogo(8)).toBe(8);
+//       expect(jogo(9)).toBe("Fizz");
+//       expect(jogo(10)).toBe("Buzz");
+//    })
+// })
+
+// describe("Teste de média", function () {
+//    it("Resultado", function () {
+//        let res = aluno();
+//        expect(res).toEqual("Reprovado")
+//    })
+// })
+
+// describe("Pessoas", function () {
+//    let nome = "Juliana";
+//    let sobrenome = "Tatin";
+//    let idade = "24";
+//
+//    let pessoa = new Pessoa(nome, sobrenome, idade);
+//
+//    it('Teste - Nome', function () {
+//        expect("Juliana").toBe(pessoa.nome)
+//    })
+//    it('Teste - Nome e Sobrenome', function () {
+//        expect("Juliana Tatin").toBe(pessoa.nome_completo)
+//    })
+//    it('Teste - Nome, Sobrenome e Idade', function () {
+//        expect("Juliana Tatin tem idade de 24").toBe(pessoa.nome_tudo())
+//    })
+// })
+
+
+//Exercício 1
+//
+// it("Multiplicador", function () {
+//    expect(multiplicador(2,4,6)).toBe(48)
+//    
+// })
+
+//Exercício 2
+//
+// it("Divisor", function () {
+//    expect(divisor(10,2)).toBe(5)
+//
+// })
+
+//Exercício 3
+
+// it("Conversor", function () {
+//    expect(conversor (32)).toBe(89.6)
+// })
+// it("Conversor2", function () {
+//    expect(converter (32)).toBe(0)
+// })
+ 
+//Exercício 4
+
+// it("Média", function () {
+//        expect(media(3,4,5)).toBe(4)
+//
+//    })
+
+//Exercício 5
+
+// it("Caractere", function () {
+//        expect(caractere("água")).toBe(4)
+//    
+//        })
+
+//Exercício 6
+
+it("Soma", function () {
+    expect (calculadora(4,2)).toBe(6)
+    })
+    it("Subtração,", function () {
+        expect(calculadora2 (4,2)).toBe(2)
+    })
+    it("Multiplicação", function () {
+       expect(calculadora3 (4,2)).toBe(8)
+    })
+    it("Divisão", function () {
+        expect(calculadora4 (4,2)).toBe(2)
+     })
---
+++ jasmine-standalone-5.4.0/src/teste.js
@@ -0,0 +1,104 @@
+//TESTE.JS AQUI VAMOS COLOCAR O CÓDIGO FONTE
+
+//function inteiro(inteiro) {
+//    return inteiro; //Essa é a solução mais simples para o erro
+// }
+
+// fizz buzz 
+// function jogo(numero){
+//
+//    if (numero %3 == 0) {
+//        return "Fizz"
+//    }
+//    if (numero %5 == 0) {
+//        return "Buzz";
+//    }
+//
+//    return numero;    
+// }
+
+// const aluno = (nota, media) => {
+//    let resultado;
+//    let desempenho = 3;
+//    if (desempenho >= 7) {
+//        resultado = "Aprovado"
+//    } else {
+//        resultado = "Reprovado"
+//    }
+//    return resultado;
+// }
+
+// class Pessoa {
+//    constructor(_Nome, _SobreNome, _Idade) {
+//
+//        let nome = _Nome;
+//        let sobrenome = _SobreNome;
+//        let idade = _Idade;
+//
+//        this.nome = () => {
+//            return nome + ' ' + sobrenome
+//        }
+//        this.nome_tudo = () => {
+//            return nome + ' ' + sobrenome + ' tm idade de ' + idade; 
+//        }
+//    }}
+
+
+    //Exercício 1
+
+//     function multiplicador ( um, dois, tres) {
+//     let resultado = um * dois * tres;
+//     return resultado
+//     }
+
+  //Exercício 2
+
+ //   function divisor ( um, dois ) {
+ //    let resultado = um / dois;
+ //    return resultado
+ //    }
+
+     //Exercício 3
+
+//    function conversor ( um ) {
+//        let resultado = um * 1.8 + 32;
+//        return resultado
+//        }
+//
+//        function converter ( dois ) {
+//            let resultado2 = (5/9) * (dois - 32)  ;
+//            return resultado2
+//            }
+
+    //Exercício 4
+
+//    function media ( um, dois, tres ) {
+//            let resultado = (um + dois + tres) / 3 ;
+//            return resultado
+//            }
+
+    //Exercício 5
+
+//    function caractere ( um ) {
+//        let resultado = um.length ;
+//        return resultado
+//        }
+
+        //Exercício 6
+
+        function calculadora ( valor1, valor2 ) {
+            let resultado1 = (valor1 + valor2 )  ;
+            return resultado1
+             }
+             function calculadora2 ( valor3, valor4 ) {
+             let resultado2 = (valor3 - valor4 )  ;
+             return resultado2
+            }
+            function calculadora3 ( valor5, valor6 ) {
+            let resultado3 = (valor5 * valor6 )  ;
+            return resultado3
+             }
+             function calculadora4 ( valor7, valor8 ) {
+            let resultado4 = (valor7 / valor8 )  ;
+            return resultado4
+             }
